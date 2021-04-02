5fdea48235267cd65c054a619e923214edede0cb
Mycellium2214 <lamofcng@gmail.com>
2021-04-02 10:24:19 +0200

    lamofc.github.io

---
+++ index.js
@@ -0,0 +1,7 @@
+let progress = document.getElementById('progressbar');
+        let totalHeight = document.body.scrollHeight - window.innerHeight;
+        
+        window.onscroll = function(){
+            let progressHeight = (window.pageYOffset / totalHeight) * 100;
+            progress.style.height = progressHeight + '%';
+        }
